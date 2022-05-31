dd5bfa534ba520e94de312347842d620bff853e4
Becca Doan <rebecca.doan@yahoo.com>
2022-05-30 21:21:42 -0500

    finised javascript password generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,8 @@
 // Assignment code here
 var lowerCaseStr = "abcdefghijklmnopqrstuvwxyz";
-var lowerCase = lowerCaseStr.split("");
-var upperCaseStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var upperCase = upperCaseStr.split("");
+var upperCaseStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberStr = "1234567890";
-var numbers = numberStr.split("");
+var specialCharStr = "~`@#$%^&*()_+=-[]{};:?/><,.";
 
 var bodyDiv = document.querySelector(".card-body");
 
@@ -14,41 +12,68 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var newFormEl = document.createElement("form");
+  // put password checklist in card body;
   bodyDiv.innerHTML = 
-            '<form>' +
             '<div class="form-group">' +
-              '<input type="checkbox" name="lowercase" value="lowercase" checked>' +
+              '<input type="checkbox" name="lowercase" id="lowercase" checked>' +
               '<label for="lowercase"> Include Lowercase</label><br>' +
-              '<input type="checkbox" id="uppercase" name="uppercase" value="uppercase" checked>' +
+              '<input type="checkbox" id="uppercase" name="uppercase" id="uppercase" checked>' +
               '<label for="uppercase"> Include Uppercase</label><br>' +
-              '<input type="checkbox" id="numbers" name="numbers" value="numbers" checked>' +
+              '<input type="checkbox" id="numbers" name="numbers" id="numbers" checked>' +
               '<label for="numbers"> Include Numbers</label><br>' +
-              '<input type="checkbox" id="special" name="special" value="special" checked>' +
+              '<input type="checkbox" id="special" name="special" id="special" checked>' +
               '<label for="special"> Include Special Characters</label><br>' +
-              '<input type="text" name="" placeholder="Enter Password Length"/>' +
+              '<input type="text"id="pw-length" placeholder="Enter Password Length"/>' +
             '</div>' +
-            '<button id="createPW" class="btn">Create Your Password</button>' +
-            '</form>';
-  // bodyDiv.innerHTML = 
-  //           '<textarea readonly id="password" placeholder= "Your Secure Password" aria-label="Generated Password"></textarea>';
-  var generatePassword = function(){
-    return "This is your password";
-  }
-  var password = generatePassword() 
-  var passwordText = document.querySelector("#password");
+            '<button id="createPW" class="btn">Create Your Password</button>';
+
+  
 
   var displayPassword = function(){
-    bodyDiv.innerHTML = 
-            '<textarea readonly id="password" placeholder= "Your Secure Password" aria-label="Generated Password"></textarea>';
+    var lowercaseInputEl = document.querySelector("#lowercase").checked;
+    var uppercaseInputEl = document.querySelector("#uppercase").checked;
+    var numberInputEl = document.querySelector("#numbers").checked;
+    var specialInputEl = document.querySelector("#special").checked;
+    var passwordLength = parseInt(document.querySelector("#pw-length").value);
+    
     var generatePassword = function(){
-        return "This is your password";
+      if(!(lowercaseInputEl || uppercaseInputEl || numberInputEl || specialInputEl)){
+        alert("must check at least one box");
+        writePassword();
+      }
+        var newPasswordArray = "";
+        if(lowercaseInputEl){
+          newPasswordArray+=lowerCaseStr;
+        }
+        if(uppercaseInputEl){
+          newPasswordArray+=upperCaseStr;
+        }
+        if(numberInputEl){
+          newPasswordArray+=numberStr;
+        }
+        if(specialInputEl){
+          newPasswordArray+=specialCharStr;
+        }
+        newPasswordArray = newPasswordArray.split("");
+        console.log(newPasswordArray.length);
+        var finalPassword = "";
+        for(var i = 0; i < passwordLength; i++){
+          var myNumber = randomNumberGenerator(0, newPasswordArray.length);
+          finalPassword+=newPasswordArray[myNumber];
+        }
+        return finalPassword;
     }
     var password = generatePassword();
+    bodyDiv.innerHTML = 
+            '<textarea readonly id="password" placeholder= "Your Secure Password" aria-label="Generated Password"></textarea>';
+
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
   }
   createPW.addEventListener("click", displayPassword);
 }
-
+function randomNumberGenerator(min,max){
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
